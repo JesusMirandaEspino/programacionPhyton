4cd8040bf97cd6ce9b4d5e5425dd7a8a3280829a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-16 23:02:28 -0500

    read archive

---
+++ semana 2/evaluacion2.py
@@ -0,0 +1,63 @@
+# Clase general para el manejo del archivo
+class read_text:
+    # Inicializan las variables
+    def __init__(self):
+        self.correo: str
+        self.dia_semana: str
+        self.dia_num: int
+        self.mes: str
+        self.anio: int
+        self.hora: str
+        self.archivo_extension: str = 'txt'
+        self.tomar_dato: str
+        self.data_archivo = ['From', 'stephen.marquard@uct.ac.za', 'Sat', 'Jan', 5, '09:14:16', 2008]
+
+    # Funcion para ingresar y verificar el archivo ingresado
+    def dato_input(self):
+
+        try:
+
+            print( 'Ingrese el nombre del archivo con su extension: ' )
+            self.tomar_dato = input()
+
+            with open(self.tomar_dato) as f:
+                self.read_data_archivo(f)
+
+        except IOError:
+            print( 'Archivo inválido, hasta luego' )
+
+    # Funcion para leer el contenido del archivo e insertarlo en las variables respectivas
+    def read_data_archivo(self, f):
+        for data in self.data_archivo:
+            if data == 'stephen.marquard@uct.ac.za':
+                self.correo = data
+            elif data == 'Sat':
+                self.dia_semana = data
+            elif data == 'Jan':
+                self.mes = data
+            elif data == 5:
+                self.dia_num = data
+            elif data == '09:14:16':
+                self.hora = data
+            elif data == 2008:
+                self.anio = data
+
+        self.print_data_archivo()
+
+    # Funcion para imprimir el resultado de leer el archivo
+    def print_data_archivo(self):
+        print( 'El correo es: ', self.correo )
+        print( 'El día en que se mandó fue: ', self.dia_semana )
+        print( 'El número de día fue: ', self.dia_num )
+        print( 'El mes en que se mandó fue: ', self.mes )
+        print( 'El año en que se mandó fue: ', self.anio )
+        print( 'La hora a la que se mandó fue: ', self.hora )
+
+        print('Que tengas un excelente dia')
+
+# Fin de la clase
+
+
+comenzar_leer = read_text()
+comenzar_leer.dato_input()
+
