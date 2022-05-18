4aec4066539653a0729427bba45f23341dcea4e2
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-17 23:14:33 -0500

    Avance evaluacion 2

--- semana 2/evaluacion2.py
+++ semana 2/evaluacion2.py
@@ -1,16 +1,15 @@
 # Clase general para el manejo del archivo
+from typing import Dict
+
+
 class read_text:
     # Inicializan las variables
     def __init__(self):
-        self.correo: str
-        self.dia_semana: str
-        self.dia_num: int
-        self.mes: str
-        self.anio: int
-        self.hora: str
-        self.archivo_extension: str = 'txt'
         self.tomar_dato: str
-        self.data_archivo = ['From', 'stephen.marquard@uct.ac.za', 'Sat', 'Jan', 5, '09:14:16', 2008]
+        self.line_archivo = []
+        self.dias = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        self.list_email_order = []
+
 
     # Funcion para ingresar y verificar el archivo ingresado
     def dato_input(self):
@@ -28,32 +27,40 @@ class read_text:
 
     # Funcion para leer el contenido del archivo e insertarlo en las variables respectivas
     def read_data_archivo(self, f):
-        for data in self.data_archivo:
-            if data == 'stephen.marquard@uct.ac.za':
-                self.correo = data
-            elif data == 'Sat':
-                self.dia_semana = data
-            elif data == 'Jan':
-                self.mes = data
-            elif data == 5:
-                self.dia_num = data
-            elif data == '09:14:16':
-                self.hora = data
-            elif data == 2008:
-                self.anio = data
 
+        for line in f:
+            line = line.strip()
+            if not line.startswith("From "):
+                continue
+            #El espacio no funciono para cortar los dos puntos se agrega la siguiente sentencia para corregirlo
+            if line.startswith("From: "):
+                continue
+            self.line_archivo.append( line.split() )
+        self.new_email_data()
         self.print_data_archivo()
 
     # Funcion para imprimir el resultado de leer el archivo
     def print_data_archivo(self):
-        print( 'El correo es: ', self.correo )
-        print( 'El día en que se mandó fue: ', self.dia_semana )
-        print( 'El número de día fue: ', self.dia_num )
-        print( 'El mes en que se mandó fue: ', self.mes )
-        print( 'El año en que se mandó fue: ', self.anio )
-        print( 'La hora a la que se mandó fue: ', self.hora )
-
         print('Que tengas un excelente dia')
+
+    def new_email_data(self):
+        
+        for email in self.line_archivo:
+            correo = dict()
+            for part in email:
+                if '@' in part:
+                    correo['Correo'] = part
+                for day in self.dias:
+                    if day in part:
+                        correo['Dia'] = part
+            self.list_email_order.append(correo)
+
+        for other in self.list_email_order:
+            print(other)
+
+        print( len(self.list_email_order))
+        print( len(self.line_archivo))
+
 
 # Fin de la clase
 
